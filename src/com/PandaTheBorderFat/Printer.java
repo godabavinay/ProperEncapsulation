46d7a3926c67d036bb4ae99506964589606d1995
CompositionInJava <godabavinay@gmail.com>
2018-08-18 17:34:49 +0530

    Added constructor to Printer class

--- src/com/PandaTheBorderFat/Printer.java
+++ src/com/PandaTheBorderFat/Printer.java
@@ -1,5 +1,18 @@
 package com.PandaTheBorderFat;
 
 public class Printer {
-    
+    private int tonerLevel = 100;
+    private int numberOfPages;
+    private boolean duplexPrinter;
+
+    public Printer(int tonerLevel, int numberOfPages, boolean duplexPrinter) {
+        if(tonerLevel >= 0 && tonerLevel <= 100)
+            this.tonerLevel = tonerLevel;
+        this.numberOfPages = numberOfPages;
+        this.duplexPrinter = duplexPrinter;
+    }
+
+    public int getTonerLevel() {
+        return tonerLevel;
+    }
 }
